8722df8e68d1858d98915145f4a61666672a849c
Jayedjb765 <jayedjb76@gmail.com>
2025-05-22 16:49:54 +0600

    update routing

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>Hellow from about </h1>
+
+        </div>
+    );
+};
+
+export default AboutPage;
